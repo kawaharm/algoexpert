18c41f4e5cc069003c2be252bd87f4d350afc036
kawaharm <mjbk295@gmail.com>
2022-05-02 21:15:16 -0700

    first commit

---
+++ array/isValidSubsequence.py
@@ -0,0 +1,19 @@
+"""
+Valid Subsequence
+Given two non-empty arrays of integers, write a function that determines 
+whether the second array is a subsequence of the first one.
+
+A subsequence of an array is a set of numbers that aren't necessarily adjacent 
+in the array but that are in the same order as they appear in the array.
+
+"""
+
+
+def isValidSubsequence(array, sequence):
+    # O(n) tine | O(1) space where n is the length of the array
+    arrIdx, seqIdx = 0, 0
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence)
