a08734670307fc9c00d7c32a6ea0418baa2db7dd
kawaharm <mjbk295@gmail.com>
2022-05-04 19:10:48 -0700

    update

--- array/isValidSubsequence.py
+++ array/isValidSubsequence.py
@@ -5,7 +5,6 @@ whether the second array is a subsequence of the first one.
 
 A subsequence of an array is a set of numbers that aren't necessarily adjacent 
 in the array but that are in the same order as they appear in the array.
-
 """
 
 
